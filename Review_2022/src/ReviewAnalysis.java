a0720e8771ce1dac55409ec726379fdc687f4dc7
smithbwlhs <smithb@wlwv.k12.or.us>
2024-05-02 10:03:43 -0700

    Initial commit

---
+++ Review_2022/src/ReviewAnalysis.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class ReviewAnalysis {
+
+    private Review[] allReviews = new Review[10];
+
+    public ReviewAnalysis(){
+        Arrays.fill(allReviews, null);
+
+    }
+
+    public double getAverageRating(){
+
+    }
+
+    public ArrayList<String> collectComments{
+
+    }
+}
